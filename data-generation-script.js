f98dce602a0712455718ef6c051e998ac7fa6147
Suhail Ansari <suhailansari@gmail.com>
2018-02-02 11:46:05 -0800

    updated data den script

--- data-generation-script.js
+++ data-generation-script.js
@@ -1,6 +1,33 @@
 const fs = require('fs');
 
-fs.writeFile('mock-data.csv', 'Hello Node.js', (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
+var writeStream = fs.createWriteStream('test-output3.csv');
+
+var startTime = 1514764800;
+var endTime = 1514851200;
+var timeInterval = 30000;
+var latMin = 37.73;
+var latMax = 37.83;
+var longMin = 122.37;
+var longMax = 122.47;
+
+for(var i = 8000000; i <= 10000000; i++) {
+  var start = [(Math.random() * (latMax - latMin) + latMin).toFixed(2) * 1, (Math.random() * (longMax - longMin) + longMin).toFixed(2) * 1];
+  var end = [(Math.random() * (latMax - latMin) + latMin).toFixed(2) * 1, (Math.random() * (longMax - longMin) + longMin).toFixed(2) * 1];
+  var distance = Math.floor(Math.random() * (7 - 1)) + 1;
+  var time1 = Math.floor(Math.random() * (endTime - startTime)) + startTime;
+  var price = Math.round(Math.random() * (25 - 4) + 4, 2);
+  var time2 = Math.floor(Math.random() * ((time1 + timeInterval) - time1)) + time1;
+  var accept = Math.random() >= 0.5;
+  var time3 = Math.floor(Math.random() * ((time2 + timeInterval) - time2)) + time2;
+  writeStream.write(i + ',' + JSON.stringify(start) + ',' + JSON.stringify(end) + ',' + time1 + ',' + price + ',' + time2 + ',' + accept + ',' + time3 + '\n');
+}
+
+writeStream.end();
+
+// jan01 = 1514764800
+// jan31 = 1517356800
+// Math.floor(Math.random() * (max - min)) + min
+// lat 37.73-37.83
+// long 122.37-122.47
+// jan01 = 1514764800
+// jan02 = 1514851200
